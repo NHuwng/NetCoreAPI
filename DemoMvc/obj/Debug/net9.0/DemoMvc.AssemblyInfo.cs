f738a6710e96592b99cdcd69ab22d51fe5c80c52
NHuwng <nnth07102003@gmail.com>
2025-01-12 02:53:35 +0700

    first commit

--- DemoMvc/obj/Debug/net9.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net9.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf4fe25b4e51c10f14aed110f169444f946a11a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
